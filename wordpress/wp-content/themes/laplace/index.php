1f03cd210a8655cb935cb8274867872aed105ac3
Linus <linusfessler@gmail.com>
2017-04-12 19:24:06 +0200

    Made pages other than menu/events non-static again

--- wordpress/wp-content/themes/laplace/index.php
+++ wordpress/wp-content/themes/laplace/index.php
@@ -1,31 +1,14 @@
 <?php get_header(); ?>
 
-<section id="first" class="white-section">
-	<article>
-		<header class="medium-header">Welcome</header>
-		<p>
-			LaPlace Restaurant was founded in May of 2015. The cuisine we serve is created with the utmost attention to details. Our emphasis is on providing fresh, locally sourced, exquisite food. As such our menus change on a regular basis, allowing us to offer you mouth watering, perfectly prepared dishes.
-		</p>
-	</article>
-</section>
+<?php
+	$pages = get_pages('sort_column=menu_order');
+?>
 
-<section class="image-section" id="highQualityCuisine" style="background-image: url(wp-content/themes/laplace/images/small1.jpg)">
-	<article>
-		<header class="medium-header">High Quality Cuisine</header>
-		<p>
-			Our cuisine is a melting pot of different cultures which have come together to form a unique blend of flavours and techniques.
-		</p>
-	</article>
-</section>
-
-<section class="white-section" id="onlyTheBestIngredients">
-	<article>
-		<header class="medium-header">Only the Best Ingredients</header>
-		<p>
-			It's vital to our operation to make sure everybody is aware of the quality of the ingredients we use. As the choices we make in terms of which supplies we buy for our recipes is intrinsic to factors such as the healthiness of the food we make to the price you pay for it. That's why on our menus you find the origins of each of our ingredients.
-		</p>
-	</article>
-</section>
+<?php
+	echo $pages[0]->post_content;
+	echo $pages[1]->post_content;
+	echo $pages[2]->post_content;
+?>
 
 <section class="brown-section" id="menu">
 	<article class="restaurant-menu">
@@ -108,21 +91,17 @@
 	</article>
 </section>
 
-<section id="events" class="white-section">
-	<article>
-		<header class="medium-header">Our Events</header>
-		<p>
-			In our Philosophy, a restaurant is not only a place where to eat but also to communicate and know new people. For these reasons we organize various events every month.
-		</p>
-	</article>
-</section>
+<?php
+	$pages = get_pages('sort_column=menu_order');
+	echo $pages[3]->post_content;
+?>
 
 <section class="brown-section">
 	<article>
 		<header class="large-header"><b>Upcoming Events</b></header>
 		<div class="upcoming-events">
 			
-			<?php $dishPosts = new WP_Query('post_type=event&posts_per_page=3&order=desc&meta_key=event_date&orderby=event_date');
+			<?php $dishPosts = new WP_Query('post_type=event&posts_per_page=3&order=DESC&meta_key=event_date&orderby=event_date');
 			if ($dishPosts->have_posts()) :
 				while ($dishPosts->have_posts()) :
 					$dishPosts->the_post();
@@ -156,7 +135,7 @@
 			<header class="large-header"> <b> Past Events </b> </header>
 			<div class="past-events">
 			
-			<?php $dishPosts = new WP_Query('post_type=event&posts_per_page=4&order=desc&meta_key=event_date&orderby=event_date&offset=3');
+			<?php $dishPosts = new WP_Query('post_type=event&posts_per_page=4&order=DESC&meta_key=event_date&orderby=event_date&offset=3');
 				if ($dishPosts->have_posts()) :
 					while ($dishPosts->have_posts()) :
 						$dishPosts->the_post();
@@ -186,48 +165,11 @@
 	</article>
 </section>
 
-<section id="contacts" name="contacts" class="white-section">
-	<article>
-		<header class="medium-header">Contact us</header>
-		<p>Feel free to contact us for any kind of issues or questions</p>
-		<form class="contact-form" method="post" action="#">
-			<div id="nameemail">
-				<input type="text" placeholder="Name" />
-				<input type="text" placeholder="Email" />
-			</div>
-			<textarea name="message" placeholder="Message"></textarea>
-			<ul>
-				<li><input type="submit" value="Send Message" /></li>
-				<li><input type="reset" value="Clear Form" /></li>
-			</ul>
-		</form>
-	</article>
-</section>
-
-<section class="image-section" id="bookATableSection" style="background-image: url(wp-content/themes/laplace/images/book-table.jpg)">
-	<article class="book-a-table-form-container">
-		<header class="medium-header">Book a table</header>
-		<div>
-			<div>
-				<form class="book-a-table-form" method="post" action="#">
-					<div style="-webkit-column-count: 2; -moz-column-count: 2; column-count: 2;">
-						<div class=""><input type="text" placeholder="Name" /></div>
-						<div><input type="text" placeholder="Phone" /></div>
-					</div>
-					<div style="-webkit-column-count: 2; -moz-column-count: 2; column-count: 2;">
-						<div><input type="text" placeholder="Date" /></div>
-						<div><input type="text" placeholder="Time" /></div>
-					</div>
-					<div>
-						<div><textarea name="message" placeholder="Message"></textarea></div>
-					</div>
-					</br>
-					<input id="btn-book" type="submit" value="Book" />
-				</form>
-			</div>
-		</div>
-	</article>
-</section>
+<?php
+	$pages = get_pages('sort_column=menu_order');
+	echo $pages[4]->post_content;
+	echo $pages[5]->post_content;
+?>
 
 <section class="white-section" id="openingHoursSection">
 	<article class="opening-hours">
