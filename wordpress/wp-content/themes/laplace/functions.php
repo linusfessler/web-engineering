29d315abb6ab97b9f0dbc4660159f2908b50f8e1
Linus <linusfessler@gmail.com>
2017-04-12 18:58:15 +0200

    Dish and event custom type

--- wordpress/wp-content/themes/laplace/functions.php
+++ wordpress/wp-content/themes/laplace/functions.php
@@ -114,9 +114,261 @@ function register_basic_settings($wp_customize){
 		'settings' => 'header_background',
 	) ) );
 }
+
 add_action('customize_register', 'register_basic_settings');
 
+add_theme_support( 'post-thumbnails' );
+
+/***** custom type dish ******/
+
+if( ! function_exists( 'create_dish_post_type' ) ):
+	function create_dish_post_type() {
+		$labels = array(
+			'name' => __( 'Dish' ),
+			'singular_name' => __( 'Dish' ),
+			'menu_name' => __( 'Dishes' ),
+			'add_new' => __( 'Add dish' ),
+			'all_items' => __( 'All dishes' ),
+			'add_new_item' => __( 'Add dish' ),
+			'edit_item' => __( 'Edit dish' ),
+			'new_item' => __( 'New dish' ),
+			'view_item' => __( 'View dish' ),
+			'search_items' => __( 'Search dishes' ),
+			'not_found' => __( 'No dishes found' ),
+			'not_found_in_trash' => __( 'No dishes found in trash' ),
+			'parent_item_colon' => __( 'Parent dish' )
+		);
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_in_nav_menus' => true,
+			'query_var' => true,
+			'rewrite' => true,
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			'supports' => array('thumbnail'),
+			'menu_position' => 5,
+			'register_meta_box_cb' => 'add_dish_post_type_metabox'
+		);
+		register_post_type( 'dish', $args );
+	}
+	add_action( 'init', 'create_dish_post_type' );
+endif;
+
+function add_dish_post_type_metabox() {
+	add_meta_box( 'dish_metabox', 'Dish Data', 'dish_metabox', 'dish', 'normal' );
+}
+
+function dish_metabox() {
+	global $post;
+	$custom = get_post_custom($post->ID);
+	$type = $custom['dish_type'][0];
+	$title = $custom['dish_title'][0];
+	$description = $custom['dish_description'][0];?>
+	<div class="dish">
+		<p> <label>Type<br> <input type="text" name="type" size="50" list="types"
+			value="<?php echo $type; ?>"> </label>
+		</p>
+		<datalist id="types">
+			<option value="Appetizers">
+			<option value="Fresh Pasta">
+			<option value="Meat - Fish">
+			<option value="Dessert">
+		</datalist>
+		
+		<p> <label>Title<br> <input type="text" name="title" size="50"
+			value="<?php echo $title; ?>"> </label>
+		</p>
+		<p> <label>Description<br> <input type="text" name="description" size="50"
+			value="<?php echo $description; ?>"> </label>
+		</p>
+	</div>
+<?php }
+
+function dish_post_save_meta( $post_id, $post ) {
+	// is the user allowed to edit the post or page?
+	if( ! current_user_can( 'edit_post', $post->ID )){
+		return $post->ID;
+	}
+	$dish_post_meta['dish_type'] = $_POST['type'];
+	$dish_post_meta['dish_title'] = $_POST['title'];
+	$dish_post_meta['dish_description'] = $_POST['description'];
+	// add values as custom fields
+	foreach( $dish_post_meta as $key => $value ) {
+		if( get_post_meta( $post->ID, $key, FALSE ) ) {
+			// if the custom field already has a value
+			update_post_meta($post->ID, $key, $value);
+		} else {
+			// if the custom field doesn't have a value
+			add_post_meta( $post->ID, $key, $value );
+		}
+		if( !$value ) {
+			// delete if blank
+			delete_post_meta( $post->ID, $key );
+		}
+	}
+}
+
+add_action('save_post', 'dish_post_save_meta', 1, 2); // save the custom fields
+
+/***** custom type event ******/
+
+if( ! function_exists( 'create_event_post_type' ) ):
+	function create_event_post_type() {
+		$labels = array(
+			'name' => __( 'Event' ),
+			'singular_name' => __( 'Event' ),
+			'menu_name' => __( 'Events' ),
+			'add_new' => __( 'Add event' ),
+			'all_items' => __( 'All events' ),
+			'add_new_item' => __( 'Add event' ),
+			'edit_item' => __( 'Edit event' ),
+			'new_item' => __( 'New event' ),
+			'view_item' => __( 'View event' ),
+			'search_items' => __( 'Search events' ),
+			'not_found' => __( 'No events found' ),
+			'not_found_in_trash' => __( 'No events found in trash' ),
+			'parent_item_colon' => __( 'Parent event' )
+		);
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'publicly_queryable' => true,
+			'show_in_nav_menus' => true,
+			'query_var' => true,
+			'rewrite' => true,
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			'supports' => array('thumbnail'),
+			'menu_position' => 5,
+			'register_meta_box_cb' => 'add_event_post_type_metabox'
+		);
+		register_post_type( 'event', $args );
+	}
+	add_action( 'init', 'create_event_post_type' );
+endif;
+
+function add_event_post_type_metabox() {
+	add_meta_box( 'event_metabox', 'Event Data', 'event_metabox', 'event', 'normal' );
+}
+
+function event_metabox() {
+	global $post;
+	$custom = get_post_custom($post->ID);
+	$title = $custom['event_title'][0];
+	$date = $custom['event_date'][0];
+	$start_time = $custom['event_start_time'][0];
+	$end_time = $custom['event_end_time'][0];
+	$description = $custom['event_description'][0];?>
+	<div class="dish">		
+		<p> <label>Title<br> <input type="text" name="title" size="50"
+			value="<?php echo $title; ?>"> </label>
+		</p>
+		<p> <label>Start Time<br> <input type="date" name="date" size="50"
+			value="<?php echo $date; ?>"> </label>
+		</p>
+		<p> <label>Start Time<br> <input type="time" name="start_time" size="50"
+			value="<?php echo $start_time; ?>"> </label>
+		</p>
+		<p> <label>End Time<br> <input type="time" name="end_time" size="50"
+			value="<?php echo $end_time; ?>"> </label>
+		</p>
+		<p> <label>Description<br> <input type="text" name="description" size="50"
+			value="<?php echo $description; ?>"> </label>
+		</p>
+	</div>
+<?php }
+
+function event_post_save_meta( $post_id, $post ) {
+	// is the user allowed to edit the post or page?
+	if( ! current_user_can( 'edit_post', $post->ID )){
+		return $post->ID;
+	}
+	$event_post_meta['event_title'] = $_POST['title'];
+	$event_post_meta['event_date'] = $_POST['date'];
+	$event_post_meta['event_start_time'] = $_POST['start_time'];
+	$event_post_meta['event_end_time'] = $_POST['end_time'];
+	$event_post_meta['event_description'] = $_POST['description'];
+	// add values as custom fields
+	foreach( $event_post_meta as $key => $value ) {
+		if( get_post_meta( $post->ID, $key, FALSE ) ) {
+			// if the custom field already has a value
+			update_post_meta($post->ID, $key, $value);
+		} else {
+			// if the custom field doesn't have a value
+			add_post_meta( $post->ID, $key, $value );
+		}
+		if( !$value ) {
+			// delete if blank
+			delete_post_meta( $post->ID, $key );
+		}
+	}
+}
+
+add_action('save_post', 'event_post_save_meta', 1, 2); // save the custom fields
+
+/*add_action('customize_register', 'register_basic_settings');
+
+add_action('admin_menu', 'setup_theme_admin_menu');
+	function setup_theme_admin_menu() {
+	add_menu_page(
+		'Dishes Theme Settings', 	// page title
+		'Dishes Settings', 			// menu title
+		'manage_options', 			// minimum user capability
+		'dishes_settings',			// menu slug
+		'dishes_settings_page' 		// function for settings page
+	);
+}
+
+function dishes_settings_page() { ?>
+	<div>
+		<form method="POST" action="">
+			<input type="hidden" name="update_settings" value="Y" />
+			<table class="form-table">
+				<tr>
+					<th><label for="dish">Dish title:</label></th>
+					<td>
+						<input type="text" name="title"
+						value="<?php print get_option("dish_title"); ?>"
+						size ="25" />
+					</td>
+				</tr>
+				<tr>
+					<th><label for="dish">Dish description:</label></th>
+					<td>
+						<input type="text" name="description"
+						value="<?php print get_option("dish_description"); ?>"
+						size ="25" />
+					</td>
+				<tr>
+				</tr>
+					<th><label for="dish">Dish image:</label></th>
+					<td>
+						<input type="text" name="image"
+						value="<?php print get_option("dish_image"); ?>"
+						size ="25" />
+					</td>
+				</tr>
+			</table>
+			<p>
+				<input type="submit" value="Save settings" class="button-primary"/>
+			</p>
+		</form>
+	</div>
+	<?php
+	if (isset($_POST["update_settings"])) {
+		$lecture = esc_attr($_POST["title"]);
+		update_option("dish_title", $lecture);
+		$exercise = esc_attr($_POST["description"]);
+		update_option("dish_description", $exercise);
+		$desc = esc_attr($_POST["image"]);
+		update_option("dish_image", $desc);
+		?>
+		<div id="message" class="updated">Settings saved</div>
+	<?php }
+}*/
 
 
-require get_template_directory() . '/inc/customizer.php';
+
 ?>
--- wordpress/wp-content/themes/laplace/script.js
+++ wordpress/wp-content/themes/laplace/script.js
@@ -1,3 +1,4 @@
+var mE = "#menu article"
 var hqcE = "#highQualityCuisine article";
 var otbiE = "#onlyTheBestIngredients article";
 var oeE = "#events article";
@@ -12,6 +13,7 @@ var fpm = "#fresh-pasta-menu"
 var mfm = "#meat-fish-menu"
 var dm = "#dessert-menu"
 
+var firstMe = true;
 var firstHqc = true;
 var firstOtbi = true;
 var firstOe = true;
@@ -21,10 +23,12 @@ var firstCu = true;
 var firstBat = true;
 var firstOhe = true;
 
+var currentCategory;
+
 $(document).ready(function () {
-	$("#picMenu").append(project.getMenu("#appetizers-menu"));
-	hideElement('#title');
-	hideElement('#first article');
+	hide(am + "-list"); hide(fpm + "-list"); hide(mfm + "-list"); hide(dm + "-list");
+	setChosenMenu(am);
+	/*hideElement(mE);
 	hideElement(otbiE);
 	hideElement(hqcE);
 	hideElement(oeE);
@@ -32,38 +36,24 @@ $(document).ready(function () {
 	hideElement(peE);
 	hideElement(cfE);
 	hideElement(batE);
-	hideElement(ohE);
-	$("#FreshPasta").hide();
-	$("#title").animate({ left: '0px' }, "slow");
-	$("#first article").animate({ left: '0px' }, "slow");
+	hideElement(ohE);*/
 
-	$("#appetizers-menu").hover(function () {
-		$("#picMenu").html("");
-		$("#picMenu").append(project.getMenu("#appetizers-menu"));
-		$(".chosen").removeClass("chosen");
-		$("#appetizers-menu").addClass("chosen");
+	$(am).hover(function () {
+		setChosenMenu(am);
 	})
-	$("#fresh-pasta-menu").hover(function () {
-		$("#picMenu").html("");
-		$("#picMenu").append(project.getMenu("#fresh-pasta-menu"));
-		$(".chosen").removeClass("chosen");
-		$("#fresh-pasta-menu").addClass("chosen");
+	$(fpm).hover(function () {
+		setChosenMenu(fpm);
 	})
-	$("#meat-fish-menu").hover(function () {
-		$("#picMenu").html("");
-		$("#picMenu").append(project.getMenu("#meat-fish-menu"));
-		$(".chosen").removeClass("chosen");
-		$("#meat-fish-menu").addClass("chosen");
+	$(mfm).hover(function () {
+		setChosenMenu(mfm);
 	})
-	$("#dessert-menu").hover(function () {
-		$("#picMenu").html("");
-		$("#picMenu").append(project.getMenu("#dessert-menu"));
-		$(".chosen").removeClass("chosen");
-		$("#dessert-menu").addClass("chosen");
+	$(dm).hover(function () {
+		setChosenMenu(dm);
 	})
 });
 
-$(window).scroll (function () {
+/*$(window).scroll (function () {
+	var animateMe = runAnimation(mE);
 	var animateHqce = runAnimation(hqcE);
 	var animateOtbie = runAnimation(otbiE);
 	var animateOee = runAnimation(oeE);
@@ -76,7 +66,11 @@ $(window).scroll (function () {
 	var animateFreshPasta = animateMenuOptions(fpm);
 	var animateMeatFish = animateMenuOptions(mfm);
 	var animateDessert = animateMenuOptions(dm);
-
+	
+	 // Our menus
+	if (animateMe == true) {
+		animateElement(mE, firstMe);
+	}
 	 // High quality cuisine
 	if (animateHqce == true) {
 		animateElement(hqcE, firstHqc);
@@ -121,7 +115,7 @@ $(window).scroll (function () {
 	if (animateDessert == true) {
 		setChosenMenu(dm);
 	}
-});
+});*/
 
 function runAnimation(section, e) {
 	var y = $(document).scrollTop();
@@ -142,27 +136,42 @@ function animateMenuOptions(section, e) {
 function setChosenMenu(element) {
 	$(".chosen").removeClass("chosen");
 	$(element).addClass("chosen");
-	$("#picMenu").html("");
-	$("#picMenu").append(project.getMenu(element));
+	
+	if (currentCategory !== undefined) {
+		hide(currentCategory);
+	}
+	currentCategory = element + "-list";
+	show(currentCategory)
 }
 
-	function animateElement(e, first)
-	{
-		if (first == true){
-			$(e).animate({ left: '0px' }, "slow");
-			first = false;
-		}
+function animateElement(e, first) {
+	if (first == true){
+		$(e).animate({ left: '0px' }, "slow");
+		first = false;
 	}
+}
 
-	function hideElement(e) {
-		$(e).css({
-			position: 'relative',
-			left: '-2000px'
-		});
-	}
+function hideElement(e) {
+	$(e).css({
+		position: 'relative',
+		left: '-2000px'
+	});
+}
+
+function hide(e) {
+	$(e).css({
+		display: 'none'
+	});
+}
+
+function show(e) {
+	$(e).css({
+		display: 'block'
+	});
+}
 
 
-(function (project, $, undefined) { 
+/*(function (project, $, undefined) { 
 
     project.createMenuElement = function(pic, id, title, text){
         return ' <div class="menu-item"><a href="#"><img src="wp-content/themes/laplace/images/'+ pic +'.jpg" alt="" /></a><div>' +
@@ -171,51 +180,40 @@ function setChosenMenu(element) {
                     '<h2>'+title+'</h2><img src="wp-content/themes/laplace/images/'+ pic +'.jpg" alt="" />' +
                     '<p>'+text+'</p></div></div> ';
     }
-    project.getMenu = function (menuName) {
+    project.getMenu = function (menuId) {
         var menu = "";
-        switch(menuName) {
-            case "#appetizers-menu":
-                menu = project.createMenuElement("pic01", "bruschette-with-tomatos", "Bruschette with Tomatoes", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
-                menu += project.createMenuElement("pic02", "green-rolls", "Green Rolls", "A healthy twist on a vegan taco, with collard green leaves standing in for taco shells, filled with chipotle sweet potatoes, fresh corn and beans.");
-                menu += project.createMenuElement("pic03", "eggplants", "Eggplants", "Karniyarik is a Turkish dish consisting of eggplant stuffed with a mix of sautéed chopped onions, garlic, black pepper, tomatoes, parsley and ground meat.");
-                menu += project.createMenuElement("pic04", "bruschette", "Bruschette", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
-                menu += project.createMenuElement("pic05", "meatballs", "Meatballs", "A meatball is ground or minced meat rolled into a small ball, sometimes along with other ingredients, such as bread crumbs, minced onion, eggs, butter, and seasoning. Meatballs are cooked by frying, baking, steaming, or braising in sauce.");
-                menu += project.createMenuElement("pic06", "spicy-beans", "Spicy Beans", "A bit of heat from red chili flakes and plenty of garlic flavor come through with these crunchy, dill-scented green beans. Spicy Beans and Wilted Greens stew: The Parmesan rind adds richness; the dried beans deliver creaminess.");        
-                break;
-            case "#fresh-pasta-menu":
-                menu = project.createMenuElement("pic11", "falafelle", "Falafelle", "A Middle Eastern dish of spiced mashed chickpeas or other pulses formed into balls or fritters and deep-fried, usually eaten with or in pitta bread.");
-                menu += project.createMenuElement("pic12", "penne", "Penne", "Pasta in the form of short wide tubes.");
-                menu += project.createMenuElement("pic13", "campanelle", "Campanelle", "Campanelle is a type of pasta which is shaped like a cone with a ruffled edge, or a bell-like flower. It is also sometimes referred to as gigli or riccioli.");
-                menu += project.createMenuElement("pic14", "angel-hair", "Angel Hair", "A type of pasta consisting of very fine long strands.");
-                menu += project.createMenuElement("pic15", "acini-di-pepe", "Acini di Pepe", "Acini di pepe are a form of pasta. They are also sometimes referred to as pastina; however, some pasta makers distinguish pastina as smaller than acini di pepe. Acini di pepe are often used in Italian wedding soup.");
-                menu += project.createMenuElement("pic16", "orecchiette", "Orecchiette", "Small pieces of ear-shaped pasta.");
-                break;
-            case "#meat-fish-menu":
-                menu = project.createMenuElement("pic21", "steak", "Steak", "High-quality beef taken from the hindquarters of the animal, typically cut into thick slices that are cooked by grilling or frying.");
-                menu += project.createMenuElement("pic22", "cheaken-wings", "Chicken wings", "A chicken wing in the cuisine of the United States is a chicken wing section (flapper or drumette) that is generally deep-fried, unbreaded, and coated in vinegar-based cayenne pepper hot sauce and butter.");
-                menu += project.createMenuElement("pic23", "shashlik", "Meat skewer", "A long piece of wood or metal used for holding pieces of food, typically meat, together during cooking. 'thread the meat on to large skewers and grill over a gentle heat.");
-                menu += project.createMenuElement("pic24", "salmon", "Salmon fillet", "A cured and then hot or cold smoked fillet.");
-                menu += project.createMenuElement("pic25", "white-fish", "Trout fillet", "A cooked trout filet.");
-                menu += project.createMenuElement("pic26", "blue-fish", "Perch", "A roasted perch.");
-                break;
-            case "#dessert-menu":
-                menu = project.createMenuElement("pic31", "capcakes", "Cupcakes", "A small cake.");
-                menu += project.createMenuElement("pic32", "pink-cake", "Pink berry cake", "A small cake with berries.");
-                menu += project.createMenuElement("pic33", "bd-cake", "Birthday cake", "A cake for your birthday.");
-                menu += project.createMenuElement("pic34", "pinkey", "Pinkie", "Pinkie Cake is a pink pony that Zane loves and he has said \"Pinkie Cake\" was his \"favorite\" pony many times in Side stories.");
-                menu += project.createMenuElement("pic35", "5-berries", "Five berries", "A berry cake with five different sorts of berries.");
-                menu += project.createMenuElement("pic36", "easter-cake", "Easter cake", "A cake with easter eggs.");
-                break;
-            default:
-                menu = project.createMenuElement("pic01", "bruschette-with-tomatos", "Bruschette with Tomatoes", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
-                menu += project.createMenuElement("pic02", "green-rolls", "Green Rolls", "A healthy twist on a vegan taco, with collard green leaves standing in for taco shells, filled with chipotle sweet potatoes, fresh corn and beans.");
-                menu += project.createMenuElement("pic03", "eggplants", "Eggplants", "Karniyarik is a Turkish dish consisting of eggplant stuffed with a mix of sautéed chopped onions, garlic, black pepper, tomatoes, parsley and ground meat.");
-                menu += project.createMenuElement("pic04", "bruschette", "Bruschette", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
-                menu += project.createMenuElement("pic05", "meatballs", "Meatballs", "A meatball is ground or minced meat rolled into a small ball, sometimes along with other ingredients, such as bread crumbs, minced onion, eggs, butter, and seasoning. Meatballs are cooked by frying, baking, steaming, or braising in sauce.");
-                menu += project.createMenuElement("pic06", "spicy-beans", "Spicy Beans", "A bit of heat from red chili flakes and plenty of garlic flavor come through with these crunchy, dill-scented green beans. Spicy Beans and Wilted Greens stew: The Parmesan rind adds richness; the dried beans deliver creaminess.");
-        }
+		menu += project.createMenuElement("pic01", "bruschette-with-tomatos", "Bruschette with Tomatoes", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
+		menu += project.createMenuElement("pic02", "green-rolls", "Green Rolls", "A healthy twist on a vegan taco, with collard green leaves standing in for taco shells, filled with chipotle sweet potatoes, fresh corn and beans.");
+		menu += project.createMenuElement("pic03", "eggplants", "Eggplants", "Karniyarik is a Turkish dish consisting of eggplant stuffed with a mix of sautéed chopped onions, garlic, black pepper, tomatoes, parsley and ground meat.");
+		menu += project.createMenuElement("pic04", "bruschette", "Bruschette", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
+		menu += project.createMenuElement("pic05", "meatballs", "Meatballs", "A meatball is ground or minced meat rolled into a small ball, sometimes along with other ingredients, such as bread crumbs, minced onion, eggs, butter, and seasoning. Meatballs are cooked by frying, baking, steaming, or braising in sauce.");
+		menu += project.createMenuElement("pic06", "spicy-beans", "Spicy Beans", "A bit of heat from red chili flakes and plenty of garlic flavor come through with these crunchy, dill-scented green beans. Spicy Beans and Wilted Greens stew: The Parmesan rind adds richness; the dried beans deliver creaminess.");
+		menu += project.createMenuElement("pic11", "falafelle", "Falafelle", "A Middle Eastern dish of spiced mashed chickpeas or other pulses formed into balls or fritters and deep-fried, usually eaten with or in pitta bread.");
+		menu += project.createMenuElement("pic12", "penne", "Penne", "Pasta in the form of short wide tubes.");
+		menu += project.createMenuElement("pic13", "campanelle", "Campanelle", "Campanelle is a type of pasta which is shaped like a cone with a ruffled edge, or a bell-like flower. It is also sometimes referred to as gigli or riccioli.");
+		menu += project.createMenuElement("pic14", "angel-hair", "Angel Hair", "A type of pasta consisting of very fine long strands.");
+		menu += project.createMenuElement("pic15", "acini-di-pepe", "Acini di Pepe", "Acini di pepe are a form of pasta. They are also sometimes referred to as pastina; however, some pasta makers distinguish pastina as smaller than acini di pepe. Acini di pepe are often used in Italian wedding soup.");
+		menu += project.createMenuElement("pic16", "orecchiette", "Orecchiette", "Small pieces of ear-shaped pasta.");
+		menu += project.createMenuElement("pic21", "steak", "Steak", "High-quality beef taken from the hindquarters of the animal, typically cut into thick slices that are cooked by grilling or frying.");
+		menu += project.createMenuElement("pic22", "cheaken-wings", "Chicken wings", "A chicken wing in the cuisine of the United States is a chicken wing section (flapper or drumette) that is generally deep-fried, unbreaded, and coated in vinegar-based cayenne pepper hot sauce and butter.");
+		menu += project.createMenuElement("pic23", "shashlik", "Meat skewer", "A long piece of wood or metal used for holding pieces of food, typically meat, together during cooking. 'thread the meat on to large skewers and grill over a gentle heat.");
+		menu += project.createMenuElement("pic24", "salmon", "Salmon fillet", "A cured and then hot or cold smoked fillet.");
+		menu += project.createMenuElement("pic25", "white-fish", "Trout fillet", "A cooked trout filet.");
+		menu += project.createMenuElement("pic26", "blue-fish", "Perch", "A roasted perch.");
+		menu += project.createMenuElement("pic31", "capcakes", "Cupcakes", "A small cake.");
+		menu += project.createMenuElement("pic32", "pink-cake", "Pink berry cake", "A small cake with berries.");
+		menu += project.createMenuElement("pic33", "bd-cake", "Birthday cake", "A cake for your birthday.");
+		menu += project.createMenuElement("pic34", "pinkey", "Pinkie", "Pinkie Cake is a pink pony that Zane loves and he has said \"Pinkie Cake\" was his \"favorite\" pony many times in Side stories.");
+		menu += project.createMenuElement("pic35", "5-berries", "Five berries", "A berry cake with five different sorts of berries.");
+		menu += project.createMenuElement("pic36", "easter-cake", "Easter cake", "A cake with easter eggs.");
+		menu += project.createMenuElement("pic01", "bruschette-with-tomatos", "Bruschette with Tomatoes", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
+		menu += project.createMenuElement("pic02", "green-rolls", "Green Rolls", "A healthy twist on a vegan taco, with collard green leaves standing in for taco shells, filled with chipotle sweet potatoes, fresh corn and beans.");
+		menu += project.createMenuElement("pic03", "eggplants", "Eggplants", "Karniyarik is a Turkish dish consisting of eggplant stuffed with a mix of sautéed chopped onions, garlic, black pepper, tomatoes, parsley and ground meat.");
+		menu += project.createMenuElement("pic04", "bruschette", "Bruschette", "Bruschetta is an antipasto (starter dish) from Italy consisting of grilled bread rubbed with garlic and topped with olive oil and salt. Variations may include toppings of tomato, vegetables, beans, cured meat, or cheese.");
+		menu += project.createMenuElement("pic05", "meatballs", "Meatballs", "A meatball is ground or minced meat rolled into a small ball, sometimes along with other ingredients, such as bread crumbs, minced onion, eggs, butter, and seasoning. Meatballs are cooked by frying, baking, steaming, or braising in sauce.");
+		menu += project.createMenuElement("pic06", "spicy-beans", "Spicy Beans", "A bit of heat from red chili flakes and plenty of garlic flavor come through with these crunchy, dill-scented green beans. Spicy Beans and Wilted Greens stew: The Parmesan rind adds richness; the dried beans deliver creaminess.");
         return menu;
     }
     
 
-    }(window.project = window.project || {}, jQuery));
+    }(window.project = window.project || {}, jQuery));*/
